66311016a966ea54a8bc9c855adc4ed07dc7957d
tangusjr <kipngenogilbert96@gmail.com>
2022-06-11 14:34:54 +0300

    commit tree 7a62fc24a85f9ccfa76cef0e2002f3b2e25d3f95

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+  }
+  //console.log(saturdayFun());
+  
+  function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+  }
+ 
+  /*function wrapAdjective(style="*"){
+  return function(adjective="special"){
+  return `You are ${style} ${adjective} ${style}! `
+  }
+  }*/
+
+  function wrapAdjective (d = "*"){
+    let result = function (a= 'a hard worker'){
+       return `You are ${d}${a}${d}!`
+    }
+    return result
+   }
+  
+  
